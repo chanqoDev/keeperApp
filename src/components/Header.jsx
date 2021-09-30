f04d74b72220b2985fbf33e1b2d52ee7910c64bf
Christian E <christianelena92@gmail.com>
2021-09-30 02:28:06 -0500

    add App.js, Footer, Header, Note

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const customStyle = {
+  backgroundColor: "#ffa801",
+};
+
+function Header() {
+  return <h1 style={customStyle}> Keeper App </h1>;
+}
+
+export default Header;
